202c723098df57694fe5e5e5e7b316886ab8b193
nikhilpandey-dev <nikhilpandey4@gmail.com>
2021-11-12 14:54:35 +0530

    Made port a const so that can change it in server/index.js.

--- src/server/index.js
+++ src/server/index.js
@@ -13,8 +13,9 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+const port = 8080;
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`)
 })
 
 app.get('/test', function (req, res) {
