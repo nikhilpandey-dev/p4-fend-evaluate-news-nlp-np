772599b078744c21e8b215b7d8b65b63f614ef9a
nikhilpandey-dev <nikhilpandey4@gmail.com>
2021-11-12 19:22:26 +0530

    Added cors and body-parser syntax as given in the lesson 3.

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,13 @@
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
-
+const cors = require('cors');
 const app = express()
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cors());
 
 app.use(express.static('dist'))
 
