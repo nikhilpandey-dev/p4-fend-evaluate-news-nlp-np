d7f333c3d26df2d025ebc3c162477308ac7e28cc
nikhilpandey-dev <nikhilpandey4@gmail.com>
2021-11-15 17:38:44 +0530

    Refined a bit the client and server code.

--- src/client/index.js
+++ src/client/index.js
@@ -3,6 +3,7 @@ import "regenerator-runtime/runtime";
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 import { postData } from "./js/formHandler";
+import { isValidURL } from "./js/formHandler";
 import './styles/resets.scss'
 import './styles/base.scss'
 import './styles/footer.scss'
@@ -14,5 +15,7 @@ alert("I exist!");
 
 export {
     checkForName,
-    handleSubmit
+    handleSubmit,
+    postData,
+    isValidURL
 }
--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -5,22 +5,29 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    postData('/article', {url: formText})
-    .then(function(data){
-        console.log("The data returned is:");
-        console.log(data);
-        const div = document.createElement('div');
-        const p = document.createElement('p');
-        const score_tag = `score_tag: ${data.score_tag}\n`;
-        const agreement = `agreement: ${data.agreement}\n`;
-        const subjectivity = `subjectivity: ${data.subjectivity}\n`;
-        const confidence = `confidence: ${data.confidence}\n`;
-        const combinedText = `${score_tag}${agreement}${subjectivity}${confidence}`
-        p.append(combinedText);
-        div.appendChild(p);
+    const resultsContainer = document.getElementById('results');
+    removeChildren(resultsContainer)
+    if (isValidURL(formText)) {
+        postData('/article', {url: formText})
+        .then(function(data){
+            console.log("The data returned is:");
+            console.log(data);
+            const div = document.createElement('div');
+            const p = document.createElement('p');
+            const score_tag = `score_tag: ${data.score_tag}\n`;
+            const agreement = `agreement: ${data.agreement}\n`;
+            const subjectivity = `subjectivity: ${data.subjectivity}\n`;
+            const confidence = `confidence: ${data.confidence}\n`;
+            const combinedText = `${score_tag}${agreement}${subjectivity}${confidence}`
+            p.innerHTML = combinedText;
+            div.appendChild(p);
+            resultsContainer.appendChild(div); 
+        }) 
+    } else {
+        resultsContainer.innerHTML = "Invalid URL. Please enter a valid URL"
+    }
 
-        document.getElementById('results').appendChild(div); 
-    })
+    
 
     Client.checkForName(formText)
 
@@ -50,3 +57,38 @@ async function postData(url = '', data = {}) {
 }
 
 export {postData}
+
+/* Valid URL Testing as per the bonus point
+Source: Check if a JavaScript string is a URL from stack overflow.
+(https://stackoverflow.com/questions/5717093/check-if-a-javascript-string-is-a-url)
+Implementation I've used: https://stackoverflow.com/a/5717133
+```javascript
+function validURL(str) {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  return !!pattern.test(str);
+}
+```
+  */
+
+function isValidURL(str) {
+    var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+      '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+    return !!pattern.test(str);
+  }
+
+export { isValidURL }
+
+function removeChildren(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.lastChild);
+    }
+} 
